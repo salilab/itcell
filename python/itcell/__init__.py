2e322801fcd8d067be0c6fa57339281f3843de7c
Ben Webb <ben@salilab.org>
2019-03-25 14:13:19 -0700

    Switch to new Wynton cluster

--- python/itcell/__init__.py
+++ python/itcell/__init__.py
@@ -2,7 +2,7 @@ import saliweb.backend
 import os, sys, stat
 
 class Job(saliweb.backend.Job):
-    runnercls = saliweb.backend.SGERunner
+    runnercls = saliweb.backend.WyntonSGERunner
 
     def run(self):
         # TODO
@@ -10,12 +10,13 @@ class Job(saliweb.backend.Job):
         input_line = par.readline().strip()
         
         script = """
+module load Sali
 module load sali-libraries\n
 perl /netapp/sali/dina/bayer/ITCell/scripts/runITCell.pl %s >& itcell.log
 """ % (input_line)
 
         r = self.runnercls(script)
-        r.set_sge_options('-l arch=linux-x64,h_rt=300:00:00,mem_free=4G -p 0')
+        r.set_sge_options('-l arch=lx-amd64,h_rt=300:00:00,mem_free=4G -p 0')
         return r
 
 
